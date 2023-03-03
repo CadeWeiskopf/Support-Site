329b40b2d8756ad683bd6494fd84cdb6b4ed83d2
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-03 18:32:54 -0500

    form submit

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,14 +1,28 @@
+const formSubmit = async () => {
+  console.log(`submit`);
+};
+
 export default function Login() {
   return (
     <>
       <h2>Login</h2>
       <div className="form-div">
-        <form className="login-form">
+        <form
+          className="login-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            formSubmit();
+          }}
+        >
           <input
+            name="email"
+            required
             type="email"
             placeholder="Email@domain.com"
           />
           <input
+            name="password"
+            required
             type="password"
             placeholder="Password"
           />
