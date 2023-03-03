f3ca4aee496568145d2114edb3d58b6abd66928f
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-03 17:46:16 -0500

    svg style, email icon

--- src/assets/PhoneIcon.tsx
+++ src/assets/PhoneIcon.tsx
@@ -2,11 +2,8 @@ export default function PhoneIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
       fill="currentColor"
       viewBox="0 0 16 16"
-      style={{ verticalAlign: "bottom" }}
     >
       <path
         fill-rule="evenodd"
--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -1,11 +1,16 @@
+import EmailIcon from "../assets/EmailIcon";
 import PhoneIcon from "../assets/PhoneIcon";
 
 export default function Contact() {
   return (
     <>
       <h2>Contact</h2>
+      <p>Please contact anytime using the information below:</p>
       <p>
-        <PhoneIcon /> (123) 123 - 1234
+        <PhoneIcon /> (123) 123-1234
+      </p>
+      <p>
+        <EmailIcon /> test@test.com
       </p>
     </>
   );
