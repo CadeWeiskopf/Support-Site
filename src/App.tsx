50882f273c6c25ae32306dcf5e67b81140679fb9
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-03 16:21:59 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-browser-router";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import About from "./screens/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <div className="main-div">
+            <Route
+              exact
+              path="/"
+              component={Home}
+            />
+            <Route
+              path="/login"
+              component={Login}
+            />
+            <Route
+              path="/about"
+              component={About}
+            />
+          </div>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
