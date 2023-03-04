a30688ff2cb98652f73d4a056cf21e09115fbcac
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-04 02:05:46 -0500

    caseData, and time tosleep

--- src/screens/Case.tsx
+++ src/screens/Case.tsx
@@ -1,10 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import AppContext from "../AppContext";
 
 export default function Case() {
   const { id, guid } = useParams();
   const { user } = useContext(AppContext);
+  // TODO: setup casedata interface, set on mount
+  const [caseData, setCaseData] = useState<null>(null);
   const navigate = useNavigate();
   useEffect(() => {
     if (user === null) {
