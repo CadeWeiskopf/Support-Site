4225db06daf579f94c20f99e3e7965d2c56c245b
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-04 11:20:59 -0500

    suitelet start

---
+++ backend/support-site-backend/src/FileCabinet/SuiteScripts/support-site/supportSiteSuitelet.js
@@ -0,0 +1,27 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(["N/log", "N/record", "N/search"], (log, record, search) => {
+  /**
+   * Defines the Suitelet script trigger point.
+   * @param {Object} scriptContext
+   * @param {ServerRequest} scriptContext.request - Incoming request
+   * @param {ServerResponse} scriptContext.response - Suitelet response
+   * @since 2015.2
+   */
+  const onRequest = (scriptContext) => {
+    scriptContext.response.addHeader({
+      name: "Access-Control-Allow-Origin",
+      value: "http://localhost:3001",
+    });
+    scriptContext.response.addHeader({
+      name: "Access-Control-Allow-Methods",
+      value: "GET,POST",
+    });
+    log.debug(`request`, scriptContext);
+    scriptContext.response.write(JSON.stringify({ ok: true }));
+  };
+
+  return { onRequest };
+});
