9fb7bdb3e41ab12f5cea0c7d0912a99270e9ce53
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-07 18:19:12 -0500

    bootstrap fun

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,25 @@
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function Home() {
   return (
     <>
       <h2>Home</h2>
       <div>
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </p>
+        <p>Welcome</p>
+        <Card bg="danger">
+          <Card.Img
+            variant="top"
+            src="https://connectedsolutionsgroup.com/wp-content/themes/yootheme/cache/csg-mobile-hero-67f87b9f.jpeg"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     </>
   );
